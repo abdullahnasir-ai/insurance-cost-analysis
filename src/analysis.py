9074628a3d160060f5bbec8c67682e200ddc00ab
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-06-29 17:14:12 +0100

    Analysis for number of children and insurance cost
    Defined a function that calculates the average insurance cost for individuals categorised by the number of children they have, which returns a string that displays this information.

--- src/analysis.py
+++ src/analysis.py
@@ -20,5 +20,22 @@ def smoker_cost(smoker, insurance_cost):
     average_non_smoker_cost = non_smoker_total / len(smoker)
     return (f"The average cost for a smoker is {average_smoker_cost}, whereas the average cost for a non-smoker is {average_non_smoker_cost}")
 
+def children_cost(num_of_children, insurance_cost):
+    total_cost_child = {}
+    for i in range(len(num_of_children)):
+        key = int(num_of_children[i])
+        if key not in total_cost_child:
+            total_cost_child[key] = 0
+        total_cost_child[key] += float(insurance_cost[i])
+    final_string = ""
+    for key, value in total_cost_child.items():
+        count = num_of_children.count((str(key)))
+        if count > 0:
+            final_string += f"For Individuals with {key} children the average cost of insurance is {(value / count):.2f}. \n"
+    return final_string
+
+
+
+
 
 
