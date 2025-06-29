264a7e7a9032a1640998a3a68ea8d268ef52b749
Abdullah Nasir <200837751+abdullahnasir-ai@users.noreply.github.com>
2025-06-29 20:12:50 +0100

    Analysing function results

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from src.analysis import *
+
+print(smoker_cost(smoker, insurance_cost))
+
+print(children_cost(num_of_children, insurance_cost))
+
+print(cost_by_sex(sex, insurance_cost))
+
+print(cost_by_age(ages, insurance_cost))
+
+print(cost_per_region(regions, insurance_cost))
